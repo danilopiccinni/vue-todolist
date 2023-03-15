9764b04fc73ddfe1e138c51c9c1ea15b42bef252
danilopiccinni <danilopiccinni@gmail.com>
2023-03-15 16:23:34 +0100

    milestone3

--- js/script.js
+++ js/script.js
@@ -26,17 +26,39 @@ createApp({
         }
       ],
 
+      nuovaCosa: '',
+
+      newToDo : {},
+
     }
   },
 
   methods: {
+    
+      rimuoviToDo(index) {
+          this.toDos.splice(index , 1)
+        },
 
-    rimuoviToDo(index) {
-      this.toDos.splice(index , 1)
-    },
+        aggiungiLaCosa() {
 
-  }
+            if (this.nuovaCosa == '') {
+                alert('inserisci una cosa da fare')
+            } else {
+                this.newToDo = {
+                    text : this.nuovaCosa,
+                    done : false,
+                },
+
+                this.toDos.push(this.newToDo)
+
+                this.nuovaCosa = ''
+            }
+        }
+    }
+}).mount('#app')
+
+    
+
 
   
-}).mount('#app')
 
